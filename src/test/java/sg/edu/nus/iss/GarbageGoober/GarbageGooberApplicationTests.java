1098ea38bda02cb3cb7f01ca2ff4db42db31e5ec
Alejandro-houlu <alejandro.hlcw@gmail.com>
2022-05-07 15:25:02 +0800

    'first'

---
+++ src/test/java/sg/edu/nus/iss/GarbageGoober/GarbageGooberApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.GarbageGoober;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageGooberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
