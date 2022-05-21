13b833c3caa12752f328f6ee564915d7e9716cf3
Alejandro-houlu <alejandro.hlcw@gmail.com>
2022-05-21 13:11:18 +0800

    heroku tryout6

--- src/main/java/sg/edu/nus/iss/GarbageGoober/Config/WebSecurityConfig.java
+++ src/main/java/sg/edu/nus/iss/GarbageGoober/Config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/images/**","/css/**","/home**","/**","/js/**","/webjars/**").permitAll()
+            .antMatchers("/images/**","/css/**","/home**","/**","/js/**","/webjars/**","/recycle/findListById/{id}").permitAll()
             .antMatchers("/home").permitAll()
             .anyRequest()
             .authenticated()
